7178e7fb3d9ee3f6703e7a079e17d1bb681119bb
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-17 16:47:12 +0000

    add category page and button that takes the user to nes sells page

--- sellmanager/routes.py
+++ sellmanager/routes.py
@@ -5,3 +5,14 @@ from sellmanager import app, db
 @app.route('/')
 def home():
     return render_template('base.html')
+
+
+@app.route('/sells_category')
+def sells_category():
+    return render_template('sells_category.html')
+
+
+
+@app.route('/new_sells')
+def new_sells():
+    return render_template('new_sells.html')
